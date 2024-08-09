5c903bf93abca620f8395b4f427cd0563e24cb9f
BlackCheppun <aghouilesilyes1@gmail.com>
2024-08-09 17:12:25 +0100

    linked the main app with the Themes, and made sure context is well transfered to read theme correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import 'Common/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,19 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Column(
-          children: [
-            Text('test'),
-          ],
-        )
-      ),
-    );
+      title: "appbar",
+        theme: AppThemes.lightTheme,
+        darkTheme: AppThemes.darkTheme,
+        themeMode: ThemeMode.dark,
+        home: Builder(
+
+        builder:(context)=>Scaffold(
+          appBar: AppBar(
+            title: Text("Traceless", style: Theme.of(context).textTheme.titleMedium),
+          ),
+          body: SafeArea(
+                child: Column(
+                  children: [
+                    Text("hello", style: Theme.of(context).textTheme.headlineLarge),
+                    Text("gray", style: Theme.of(context).textTheme.titleSmall),
+                    Text("this is a dumb thing", style: TextStyle(color: Theme.of(context).colorScheme.primary),)
+                  ],
+                )),
+          )
+
+        ));
   }
 }
-
