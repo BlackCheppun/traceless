856d8a503d3836c771f704a7f7d13507bd0726f6
BlackCheppun <aghouilesilyes1@gmail.com>
2024-08-09 17:11:24 +0100

    corrected document, linked textTheme styles, added necessary propreties and color Scheme

--- lib/Common/theme/theme.dart
+++ lib/Common/theme/theme.dart
@@ -1,53 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:traceless/Common/theme/CustomThemes/TextThemes.dart';
 
 class AppThemes {
+  AppThemes._();
+
   //lightTheme
   static final ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    textTheme: TextTheme(
-      titleLarge: GoogleFonts.roboto(
-          color: Colors.black, fontSize: 36, fontWeight: FontWeight.bold),
-      titleMedium: GoogleFonts.roboto(
-          color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500),
-      titleSmall: GoogleFonts.roboto(
-          color: const Color(0xffB3B3B3),
-          fontSize: 14,
-          fontWeight: FontWeight.w400),
-      bodyMedium: GoogleFonts.roboto(
-          color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
-      bodySmall: GoogleFonts.roboto(
-          color: Colors.black, fontSize: 14, fontWeight: FontWeight.normal),
-    ),
-    colorScheme: const ColorScheme.light(
+      useMaterial3: true,
+      fontFamily: "Poppins",
       brightness: Brightness.light,
-      background: Color(0xffFFFFFF),
-      primary: Color(0xffFFBA00),
-      secondary: Color(0xffb3b3b3),
-    ),
-  );
+      colorScheme: ColorScheme(
+        brightness: Brightness.light,
+        background: HexColor("#ffffff"),
+        onBackground: HexColor("#999999"),
+        surface: Colors.white,
+        onSurface: Colors.white,
+        primary: HexColor("#FFB100"),
+        onPrimary: HexColor("FF8100"),
+        secondary: HexColor("#B3B3B3"),
+        onSecondary: HexColor("#B3B3B3"),
+        error: HexColor("#FF1C1C"),
+        onError: HexColor("#FF1C1C"),
+      ),
+      scaffoldBackgroundColor: HexColor("#FFFFFF"),
+      textTheme: TextThemes.lightTextStyle,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+      ));
 
   //darkTheme
   static final ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    textTheme: TextTheme(
-      titleLarge: GoogleFonts.roboto(
-          color: Colors.white, fontSize: 36, fontWeight: FontWeight.bold),
-      titleMedium: GoogleFonts.roboto(
-          color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500),
-      titleSmall: GoogleFonts.roboto(
-          color: const Color(0xffB3B3B3),
-          fontSize: 14,
-          fontWeight: FontWeight.w400),
-      bodyMedium: GoogleFonts.roboto(
-          color: Colors.white, fontSize: 16, fontWeight: FontWeight.normal),
-      bodySmall: GoogleFonts.roboto(
-          color: Colors.white, fontSize: 14, fontWeight: FontWeight.normal),
-    ),
-    colorScheme: const ColorScheme.dark(
+      useMaterial3: true,
+      fontFamily: "Roboto",
+      brightness: Brightness.dark,
+      colorScheme: ColorScheme(
         brightness: Brightness.dark,
-        background: Color(0XffB3B3B3),
-        primary: Color(0XffFFBA00),
-        secondary: Color(0xffb3b3b3)),
-  );
+        background: HexColor("#333333"),
+        onBackground: HexColor("#4A4A4A"),
+        surface: Colors.black,
+        onSurface: Colors.black,
+        primary: HexColor("#FFB100"),
+        onPrimary: HexColor("FF8100"),
+        secondary: HexColor("#B3B3B3"),
+        onSecondary: HexColor("#B3B3B3"),
+        error: HexColor("#FF1C1C"),
+        onError: HexColor("#FF1C1C"),
+      ),
+      scaffoldBackgroundColor: HexColor("#333333"),
+      textTheme: TextThemes.darkTextStyle,
+      appBarTheme: const AppBarTheme().copyWith(
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+      ));
 }
